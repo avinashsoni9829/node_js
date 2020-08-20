63f012c3a18385bf7305a9bef099b46217780a5f
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-08-20 11:13:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var new_function=require('./stuff');
+
+console.log(new_function.counter(['a','b','c','d','e','f']));
+console.log(new_function.add(6,3));
+console.log(new_function.pi);
---
+++ stuff.js
@@ -0,0 +1,20 @@
+var counter=function(arr)
+{
+    return 'array has '+ arr.length +' element ';
+};
+
+var add=function(a,b)
+{
+    return `the sum is : ${a+b}`;
+};
+
+var pi=3.142;
+module.exports={
+    counter:counter ,
+    add:add ,
+    pi:pi
+};
+
+
+
+
