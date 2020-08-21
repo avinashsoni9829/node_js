e9e5a09b76e43eed22aee51c4924259610842b42
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-08-21 19:13:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-var new_function=require('./stuff');
+var http=require('http');
+var fs=require('fs');
 
-console.log(new_function.counter(['a','b','c','d','e','f']));
-console.log(new_function.add(6,3));
-console.log(new_function.pi);
+
+var myReadStream=fs.createReadStream(__dirname + '/readme.txt','utf8');
+
+var myWriteStream=fs.createWriteStream(__dirname+'/writeMe.txt');
+
+
+myReadStream.on('data',function(chunk){
+    
+    console.log('new chunk recieved:');
+    
+    myWriteStream.write(chunk);
+
+});
+
+
+
+/*var http=require('http');
+
+var server=http.createServer(function(req,res)
+{
+    console.log('req at'+req.url);
+    res.writeHead(200,{
+        'Content-Type':'text/plain'
+    });
+    res.end('hi bro!');
+});
+
+server.listen(3000,'127.0.0.1');
+console.log('server connected !');
+*/
