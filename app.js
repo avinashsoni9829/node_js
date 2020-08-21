11d85581746cd3e626dd51dd6a1c58c314096fd6
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-08-21 16:04:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var new_function=require('./stuff');
+var fs=require('fs');
 
-console.log(new_function.counter(['a','b','c','d','e','f']));
-console.log(new_function.add(6,3));
-console.log(new_function.pi);
+fs.unlink('./stuff/newfile.txt',function()
+{
+    fs.rmdir('stuff',function(err)
+    {
+        if(err)
+        throw err;
+        
+    });
+});
+
+
+
+
