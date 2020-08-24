d4e1dbec2166a590a6df3b4fdad35bd7d93f8613
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-08-24 18:36:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-var new_function=require('./stuff');
+var http=require('http');
+var fs=require('fs');
 
-console.log(new_function.counter(['a','b','c','d','e','f']));
-console.log(new_function.add(6,3));
-console.log(new_function.pi);
+
+var server=http.createServer(function(req,res)
+{
+    console.log('req at'+req.url);
+    res.writeHead(200,{
+        'Content-Type':'application/json'
+});
+
+var myObj={
+    name:'binod',
+    job:'sleep',
+    age:100
+};
+
+res.end(JSON.stringify(myObj));  
+//.end method either takes a string or a buffer in the parameters!  so we cant directly send the object we have created
+// so we use the json.stringify method to convert the object into the string 
+
+
+
+});
+
+server.listen(3000,'127.0.0.1');
+console.log('server connected !');
