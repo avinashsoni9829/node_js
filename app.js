12c8c23b568bd4e15fef0dbd1b12094ea0dbfc50
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-08-25 11:49:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-var new_function=require('./stuff');
+var express=require('express');
 
-console.log(new_function.counter(['a','b','c','d','e','f']));
-console.log(new_function.add(6,3));
-console.log(new_function.pi);
+var app=express();
+//setting ejs as our view engine 
+    
+app.set('view engine','ejs');
+//middle ware
+app.use('/assets',express.static('assets'));
+
+app.get('/',function(req,res)
+{
+    res.render('index');
+});
+
+
+app.get('/contact',function(req,res)
+{
+    res.render('contact');
+});
+
+// in order to pass data to a view we need to pass it as an object 
+app.get('/profile/:id',function(req,res) {
+    var data={age:21, job:'sleep',hobbies:['eating','sleeping','more sleeping']};
+    
+    res.render('profile',{person: req.params.id,data:data});
+
+
+});
+
+app.listen(3000);
+
+
+
+
