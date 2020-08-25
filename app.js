673a5075e5769c5cdebacd8ec0d2f3dd2d56cd08
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-08-25 13:08:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express=require('express');
-
+var bodyParser=require('body-parser');
 var app=express();
 //setting ejs as our view engine 
+//this is used to get the data from the form !
+
+var urlencodedParser=bodyParser.urlencoded({extended:false});
+
     
 app.set('view engine','ejs');
 //middle ware
@@ -13,10 +17,20 @@ app.get('/',function(req,res)
 });
 
 
+
+
 app.get('/contact',function(req,res)
 {
+    
     res.render('contact',{qs:req.query});
 });
+
+app.post('/contact',urlencodedParser,function(req,res)
+{
+    console.log(req.body);
+    res.render('contact-success',{data: req.body});
+});
+
 
 // in order to pass data to a view we need to pass it as an object 
 app.get('/profile/:id',function(req,res) {
